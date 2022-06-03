e17594814dcb7cb43df9dac161a74037cae03951
Martin De Ninnis <mdeninnis>
2022-06-03 17:44:39 +0200

    Initial Commit

---
+++ Dicee UI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Dicee UI
+//
+//  Created by Martin Dn on 3/6/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Dicee UI/Dicee_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee_UIApp.swift
+//  Dicee UI
+//
+//  Created by Martin Dn on 3/6/22.
+//
+
+import SwiftUI
+
+@main
+struct Dicee_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
